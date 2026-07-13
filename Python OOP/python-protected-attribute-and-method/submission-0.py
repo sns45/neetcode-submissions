879f2507ecb0ada3c0860dd857d3d6164071a28b
Shantanu Sharma <notifyshantanu@gmail.com>
2026-07-13 10:19:44 -0400

    Add: python-protected-attribute-and-method - submission-0

---
+++ Python OOP/python-protected-attribute-and-method/submission-0.py
@@ -0,0 +1,14 @@
+class Account:
+    def __init__(self, title: str, balance: int):
+        self.title = title
+        self._balance = balance
+        pass
+    
+    def display_balance(self) -> None:
+        print(f"Balance: ${self._balance}")
+        pass
+
+
+# Do not modify the code below this line
+account = Account("John", 1000)
+account.display_balance()
