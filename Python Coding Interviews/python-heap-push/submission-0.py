94bb179ab77a1b775c0f321b53913390f4c3b9d4
Shantanu Sharma <notifyshantanu@gmail.com>
2026-07-15 12:32:04 -0400

    Add: python-heap-push - submission-0

---
+++ Python Coding Interviews/python-heap-push/submission-0.py
@@ -0,0 +1,18 @@
+import heapq
+from typing import List
+
+
+def heap_push(heap: List[int], value: int) -> int:
+    heap = heap
+    heapq.heappush(heap, value)
+    return heap[0]
+
+    pass
+
+
+# do not modify below this line
+print(heap_push([1, 2, 3], 4))
+print(heap_push([1, 2, 3], 0))
+print(heap_push([1, 2, 3], 2))
+print(heap_push([4, 6, 7, 8, 12, 9, 10], 2))
+print(heap_push([4, 6, 7, 8, 12, 9, 10], 5))
